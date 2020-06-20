54de3fc0bd522fd63b0cb4e3a76b33d605a15b0e
Raphael <paiva.raphaelt@gmail.com>
2020-06-21 01:05:14 +0200

    add route to get size by id

--- src/Routes/SizeRoute.js
+++ src/Routes/SizeRoute.js
@@ -8,3 +8,9 @@ anonymeRouteSize.route('/')
         const Sizes = await Size.getAllSize();
         res.json(Sizes);
     })
+anonymeRouteSize.route('/:id')
+    .get(async (req, res) => {
+        const idSize = {id: req.params.id};
+        const size = await Size.getSizeById(idSize);
+        res.json(size);
+    })
