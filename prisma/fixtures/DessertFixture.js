be9a53148b36813166be0e48c7fa4b7af82ba6f0
Alex <alex.rigueur@gmail.com>
2020-06-26 13:44:03 +0200

    add more fixtures & add img

--- prisma/fixtures/DessertFixture.js
+++ prisma/fixtures/DessertFixture.js
@@ -4,16 +4,19 @@ async function dessertFixture() {
     await prisma.createDessert({
         price: 3.50,
         name: 'Tiramisu nutella',
+        img: 'https://img.cuisineaz.com/610x610/2016-02-07/i79723-tiramisu-au-nutella.jpg'
     })
 
     await prisma.createDessert({
         price: 3.50,
         name: 'Tiramisu oreo',
+        img: 'https://www.yumelise.fr/wp-content/uploads/2019/08/tiramisu-oreo-2.jpg'
     })
 
     await prisma.createDessert({
         price: 2.50,
-        name: 'Tiramisu',
+        name: 'Fondant au chocolat',
+        img: 'https://i-reg.unimedias.fr/sites/art-de-vivre/files/styles/recipe/public/Import/coulant-fondant-chocolat_ss.jpg?auto=compress%2Cformat&crop=faces%2Cedges&cs=srgb&fit=crop&h=500&w=393'
     })
 }
 dessertFixture().catch(e => console.error(e));
--- prisma/fixtures/PizzaFixture.js
+++ prisma/fixtures/PizzaFixture.js
@@ -4,24 +4,102 @@ async function pizzaFixture() {
 
     await prisma.createPizza({
         name: 'La cannibale',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PORI/FR_PORI_fr_hero_3744.png?v-666378964',
         composition: {
             sauce: 'tomato',
             cheese: true
         },
         size:{
             connect: {
-                id: 'ckbo9eit200g40759dhumypx2'
+                id: 'ckbtkq7jk000q0777hqivubjr'
             }
         },
         category: {
             connect: {
-                id: 'ckbodlnip00kx0759qsin9irc'
+                id: 'ckbtkq4mn000g07770nk2mztu'
             }
         }
     })
 
     await prisma.createPizza({
-        name: 'La vegetarienne',
+        name: 'Boursin',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PBOU/FR_PBOU_fr_hero_4205.jpg?v-650703197',
+        composition: {
+            sauce: 'tomato',
+            cheese: true
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7jk000q0777hqivubjr'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4mn000g07770nk2mztu'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Vegan Peppina',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PVPE/FR_PVPE_fr_hero_3117.png?v-550621428',
+        composition: {
+            sauce: 'tomato',
+            cheese: true
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7jk000q0777hqivubjr'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4mn000g07770nk2mztu'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Pepperoni',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PDIP/FR_PDIP_fr_hero_3744.png?v1515771323',
+        composition: {
+            sauce: 'tomato',
+            cheese: true
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7jk000q0777hqivubjr'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4mn000g07770nk2mztu'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Kebab',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PRYK/FR_PRYK_fr_hero_3555.png?v-1007165671',
+        composition: {
+            sauce: 'tomato',
+            cheese: true
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7jk000q0777hqivubjr'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4mn000g07770nk2mztu'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Bacon Groovy',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PBCG/FR_PBCG_fr_hero_2762.png?v-1641942369',
         composition: {
             sauce: 'tomato',
             cheese: true,
@@ -29,12 +107,72 @@ async function pizzaFixture() {
         },
         size:{
             connect: {
-                id: 'ckbo9eit200g40759dhumypx2'
+                id: 'ckbtkq7no00100777z3j9whgx'
             }
         },
         category: {
             connect: {
-                id: 'ckbodlnip00kx0759qsin9irc'
+                id: 'ckbtkq4po000l07778y7y2wrg'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Savoyarde',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PSVY/FR_PSVY_fr_hero_2047.png?v-1921701074',
+        composition: {
+            sauce: 'tomato',
+            cheese: true,
+            viande: 'poulet'
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7no00100777z3j9whgx'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4po000l07778y7y2wrg'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Saumon',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PSSE/FR_PSSE_fr_hero_2142.png?v669834368',
+        composition: {
+            sauce: 'tomato',
+            cheese: true,
+            viande: 'poulet'
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7no00100777z3j9whgx'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4po000l07778y7y2wrg'
+            }
+        }
+    })
+
+    await prisma.createPizza({
+        name: 'Orientale',
+        img: 'https://www.dominos.fr/ManagedAssets/FR/product/PORI/FR_PORI_fr_hero_3744.png?v-666378964',
+        composition: {
+            sauce: 'tomato',
+            cheese: true,
+            viande: 'poulet'
+        },
+        size:{
+            connect: {
+                id: 'ckbtkq7no00100777z3j9whgx'
+            }
+        },
+        category: {
+            connect: {
+                id: 'ckbtkq4po000l07778y7y2wrg'
             }
         }
     })
